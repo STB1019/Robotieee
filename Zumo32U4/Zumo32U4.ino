61efed9efb559994020a79fa6da8a480830bb301
Massimo Bono <mbono@unibs.it>
2018-02-19 13:04:52 +0100

    create standard main file in Arduino project

--- Zumo32U4/Zumo32U4.ino
+++ Zumo32U4/Zumo32U4.ino
@@ -1,11 +1,17 @@
-#include <fixed_list.hpp>
+
+#define AVR_BUILD
+#include "model.hpp"
 
 using namespace robo_utils;
+using namespace robotieee;
+
+static model zumo_model = model{10, 10};
 
 void setup() {
   // put your setup code here, to run once:
 
-  abstract_list<int>* al = new fixed_list<int>{0, 10};
+  zumo_model.zumo_robot.move(object_movement::UP);
+  
 }
 
 void loop() {

c7141287e6b48a029b4cde6852dec42bbdae90de
Lorenzo Nodari <lorenzo.nodari.it@ieee.org>
2018-02-27 22:48:16 +0100

    Fix compilation errors

--- Zumo32U4/model.hpp
+++ Zumo32U4/model.hpp
@@ -15,6 +15,7 @@
 #include <list.hpp>
 #include <matrix.hpp>
 #include <point.hpp>
+#include "block.hpp"
 #include "robot.hpp"
 
 /**
@@ -88,7 +89,7 @@ typedef unsigned int cell_content;
     /**
      * The list of all blocks in robotieee::model::workspace discovered so far
      */
-		list<block> blocks;
+		point_list blocks;
     /**
      * The list of places where the goals are
      * 

3fba1d09d1ca7819a418b4d8d96684529a5330be
Nicola Onofri <nicola.onofri@gmail.com>
2018-03-04 17:29:22 +0100

    Minor update: changed "pos" tags to JSON dict instead of list
    Sounds better to me. Also changed indent to 4 spaces

--- Server/planner_wrapper/planner_wrapper/solution_converter.py
+++ Server/planner_wrapper/planner_wrapper/solution_converter.py
@@ -1,18 +1,6 @@
 import json
-import typing
 import collections
 import re
-
-# ; Version LPG-td-1.0
-# ; Seed 89065539
-# ; Command line: /home/koldar/Documents/LPGFrancesco/LPG/lpg -n 1 -f /home/koldar/git/Robotieee/Server/planner_wrapper/Problems/Sokoban/instance-1 -o /home/koldar/git/Robotieee/Server/planner_wrapper/Problems/Sokoban/domain.pddl -out /home/koldar/git/Robotieee/Server/planner_wrapper/tests/computed.plan -onlybestfirst 
-# ; Problem /home/koldar/git/Robotieee/Server/planner_wrapper/Problems/Sokoban/instance-1
-# ; Actions having STRIPS duration
-# ; Time 4.28
-# ; Search time 4.27
-# ; Parsing time 0.01
-# ; Mutex time 0.00
-# ; NrActions 211
 
 action_tags = {
     'move': ('action','player','start-pos','end-pos','direction'),
@@ -41,7 +29,7 @@ def convert_positions_tag(positions):
     coords = ('x', 'y')
     for pos in positions:
         values = re.sub(r'^pos|-0', ' ', pos).lstrip().split()
-        res.append(list(zip(coords, values)))
+        res.append(dict(zip(coords, values)))
     return res
 
 def plan_to_dict(file_in):
@@ -62,13 +50,7 @@ def plan_to_dict(file_in):
 
 def dict_to_json(d, file_out):
     with open(file_out, 'w') as f:
-        json.dump(d,f,indent=2)
+        json.dump(d,f,indent=4)
             
-p = plan_to_dict(r"C:\Users\Nicola\Desktop\Ubuntu Backup\Robotieee-master\Server\planner_wrapper\tests\test-instance.sol")
-dict_to_json(p, r"C:\Users\Nicola\Desktop\Ubuntu Backup\Robotieee-master\Server\planner_wrapper\tests\plan.json")
-
-# ISSUES:
-#     1) I'm expecting a method that gets as input param 'computed.plan_x.SOL' and parses it. Where is it?/ Where should I put it?
-#     2) Silly question: I understand (not completely) the planner.py module, but is it really necessary? I know it is not as elegant
-#     as the current solution, but I would use a HUGE switch-case construct while iterating throu the file, line by line, rather than using such complex architecture.
-#     3) I still can't figure out how the entire program should be launched and how the modules are executed.
+p = plan_to_dict(r"C:\Users\Nicola\Desktop\ZUMOrobot\Robotieee\Server\planner_wrapper\tests\test-instance.sol")
+dict_to_json(p, r"C:\Users\Nicola\Desktop\ZUMOrobot\Robotieee\Server\planner_wrapper\tests\plan.json")

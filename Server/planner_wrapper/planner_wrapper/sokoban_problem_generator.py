48325c225d085da52fe77a6530c5f52f864e811a
Massimo Bono <mbono@unibs.it>
2018-03-07 12:08:24 +0100

    make the planner work

--- Server/planner_wrapper/planner_wrapper/sokoban_problem_generator.py
+++ Server/planner_wrapper/planner_wrapper/sokoban_problem_generator.py
@@ -1,6 +1,7 @@
 import os
 
 from planner_wrapper.point import Point
+from planner_wrapper.sokoban_actions import Direction
 
 __doc__ = """
 Given a sokoban world, creates both a problem filename representing said world 
@@ -9,6 +10,29 @@ Given a sokoban world, creates both a problem filename representing said world
 import typing
 
 from planner_wrapper import sokoban_world
+
+
+class TabFile:
+
+    def __init__(self, f, indent: int = 0, tabs_instead_of_spaces: bool = True):
+        self.f = f
+        self.indent = indent
+        self.tabs_instead_of_spaces = tabs_instead_of_spaces
+        self.new_line = True
+
+    def write(self, string: str = ""):
+        if self.new_line:
+            if self.tabs_instead_of_spaces:
+                self.f.write('\t' * self.indent)
+            else:
+                self.f.write(' ' * self.indent)
+        self.f.write(string)
+        self.new_line = False
+
+    def writeln(self, string: str = ""):
+        self.write(string)
+        self.f.write('\n')
+        self.new_line = True
 
 
 class Clause:
@@ -23,7 +47,7 @@ class Clause:
         :param fake: if true we won't write anything on the file
         :param carriage_return: if true we will append a carriage return after the clause has terminated
         """
-        self.f = f
+        self.tf = TabFile(f, indent=indent)
         self.name = name
         self.colon = colon
         self.fake = fake
@@ -32,25 +56,27 @@ class Clause:
 
     def __enter__(self):
         if self.fake:
-            return self
+            return self.tf
 
         if self.colon:
-            self.f.write("{}(:".format('\t'*self.indent))
+            self.tf.write("(:")
         else:
-            self.f.write("{}(".format('\t'*self.indent))
-        if self.name is not None:
-            self.f.write(self.name)
-            self.f.write(" ")
+            self.tf.write("(")
 
-        return self
+        if self.name is not None:
+            self.tf.write(self.name)
+            self.tf.write(" ")
+
+        return self.tf
 
     def __exit__(self, t, value, traceback):
         if self.fake:
             return
 
-        self.f.write(")")
         if self.carriage_return:
-            self.f.write("\n")
+            self.tf.writeln(")")
+        else:
+            self.tf.write(")")
 
     @staticmethod
     def write_predicate(f, name: str, value: typing.Union[str, typing.Iterable[str]]):
@@ -61,11 +87,11 @@ class Clause:
         :param value: the values of the rpedicate. It can either be a string (predicate with a single value) or a list of them
                 (multi value predicate)
         """
-        with Clause(f, indent=0, name=name, carriage_return=False):
+        with Clause(f, indent=0, name=name, carriage_return=False) as tf:
             if type(value) == str:
-                f.write(value)
+                tf.write(value)
             else:
-                f.write(" ".join(value))
+                tf.write(" ".join(value))
 
 
 class IPddlSokobanConverter:
@@ -88,91 +114,149 @@ class PddlSokobanConverterVersion1(IPddlSokobanConverter):
     def cell_predicate(self, p: Point) -> str:
         return f"cell-{p.y:02d}-{p.x:02d}"
 
+    def get_direction(self, p_start: Point, p_end: Point) -> Direction:
+        if p_start.x - p_end.x == 0:
+            #moving along y
+            if p_start.y < p_end.y:
+                return Direction.DOWN
+            elif p_start.y > p_end.y:
+                return Direction.UP
+            else:
+                raise ValueError(f"invalid point {p_start} and next point {p_end}")
+        elif p_start.y - p_end.y == 0:
+            #moving along x
+            if p_start.x < p_end.x:
+                return Direction.RIGHT
+            elif p_start.x > p_end.x:
+                return Direction.LEFT
+            else:
+                raise ValueError(f"invalid point {p_start} and next point {p_end}")
+        else:
+            raise ValueError(f"invalid point {p_start} and next point {p_end}")
+
     def generate_problem(self, problem_filename: str, domain_name: str, problem_name: str, world: sokoban_world.SokobanWorld) -> str:
         with open(problem_filename, "w") as f:
-            with Clause(f, indent=0, name="define"):
+            with Clause(f, indent=0, name="define") as tf1:
 
-                with Clause(f, indent=1, name="problem"):
-                    f.write(f"{problem_name}")
+                with Clause(f, indent=1, name="problem", carriage_return=True) as tf2:
+                    tf2.write(f"{problem_name}")
 
-                with Clause(f, indent=1, name="domain", colon=True):
-                    f.write(f"{domain_name}")
+                with Clause(f, indent=1, name="domain", colon=True, carriage_return=True) as tf2:
+                    tf2.write(f"{domain_name}")
 
-                with Clause(f, indent=1, name="objects", colon=True):
+                with Clause(f, indent=1, name="objects", colon=True, carriage_return=True) as tf2:
                     # write the 4 direction sokoban can move on
-                    f.write("dir-down - direction\n")
-                    f.write("dir-left - direction\n")
-                    f.write("dir-right - direction\n")
-                    f.write("dir-up - direction\n")
-                    f.write("\n")
+                    tf2.writeln("{} - direction".format(Direction.LEFT.parsable_str))
+                    tf2.writeln("{} - direction".format(Direction.RIGHT.parsable_str))
+                    tf2.writeln("{} - direction".format(Direction.UP.parsable_str))
+                    tf2.writeln("{} - direction".format(Direction.DOWN.parsable_str))
+                    tf2.writeln()
 
                     # player
-                    f.write("player-01 - player\n")
-                    f.write("\n")
+                    tf2.writeln("player-01 - player")
+                    tf2.writeln()
 
                     # the objects in sokoban are the traversable cells
-                    f.write("\n".join(
-                        # convert cells left in a string
-                        map(
-                            # filter out cells in the world which are untraversable
-                            lambda p: "{} - location".format(self.cell_predicate(p)),
-                            filter(
-                                lambda p2: sokoban_world.BaseCellContent.UNTRAVERSABLE not in world[p2],
-                                world.cells
-                            )
+                    locations = map(
+                        # filter out cells in the world which are untraversable
+                        lambda p: "{} - location".format(self.cell_predicate(p)),
+                        filter(
+                            lambda p2: sokoban_world.BaseCellContent.UNTRAVERSABLE not in world[p2],
+                            world.cells
                         )
-                    ))
-                    f.write("\n")
+                    )
+                    for location in locations:
+                        tf2.writeln(location)
+                    tf2.writeln()
 
                     # blocks
-                    f.write("\n".join(
-                        map(
-                            lambda x: f"stone-{x:02d} - stone",
-                            range(len(world.blocks))
-                        )
-                    ))
-                    f.write("\n")
+                    blocks = map(
+                        lambda x: f"stone-{x:02d} - stone",
+                        range(len(world.blocks))
+                    )
+                    for block in blocks:
+                        tf2.writeln(block)
+                        tf2.writeln()
 
-                with Clause(f, indent=1, name="init", colon=True):
+                with Clause(f, indent=1, name="init", colon=True) as  tf2:
                     # position of the robot
-                    f.write("\n")
+                    tf2.writeln()
                     for cell in world.cells:
-                        if not world.is_traversable(*cell):
+                        if not world.is_traversable(row=cell.y, col=cell.x):
                             continue
-                        f.write(";; CELL: y={} x={}\n".format(cell.y, cell.x))
+                        tf2.writeln(";; *****************************************")
+                        tf2.writeln(";; CELL: y={} x={}\n".format(cell.y, cell.x))
+                        tf2.writeln(";; *****************************************")
+
+                        #cell contains a robot
                         if sokoban_world.BaseCellContent.ROBOT in world[cell]:
-                            Clause.write_predicate(f, name="has_player", value=self.cell_predicate(cell))
-                            f.write("\n")
+                            Clause.write_predicate(f, name="at", value=[
+                                "player-01",
+                                self.cell_predicate(cell)
+                            ])
+                            tf2.write("\n")
+
+                        #cell contains a block
                         if sokoban_world.BaseCellContent.BLOCK in world[cell]:
-                            Clause.write_predicate(f, name="has_box", value=self.cell_predicate(cell))
-                            f.write("\n")
+                            blocks = list(filter(lambda x: x.position == cell, world.blocks))
+                            if len(blocks) != 1:
+                                raise ValueError(f"problem while looking for block in cell {cell.y}:{cell.x}. It should be only one but instead it's: {blocks}")
+                            block = blocks[0]
+                            Clause.write_predicate(f, name="at", value=[
+                                block.to_pddl_predicate(),
+                                self.cell_predicate(cell)
+                            ])
+                            tf2.writeln()
+
+                        #cell contains a goal
                         if sokoban_world.BaseCellContent.GOAL in world[cell]:
                             Clause.write_predicate(f, name="IS-GOAL", value=self.cell_predicate(cell))
-                            f.write("\n")
+                            tf2.writeln()
+
+                        #cell does not contain a goal
                         if sokoban_world.BaseCellContent.GOAL not in world[cell]:
                             Clause.write_predicate(f, name="IS-NONGOAL", value=self.cell_predicate(cell))
-                            f.write("\n")
+                            tf2.writeln()
 
+                        #cell contains both a block and a goal
+                        if sokoban_world.BaseCellContent.GOAL in world[cell] and sokoban_world.BaseCellContent.BLOCK in world[cell]:
+                            blocks = list(filter(lambda x: x.position == cell, world.blocks))
+                            if len(blocks) != 1:
+                                raise ValueError(
+                                    f"problem while looking for block in cell {cell.y}:{cell.x}. It should be only one but instead it's: {blocks}")
+                            block = blocks[0]
+                            Clause.write_predicate(f, name="at-goal", value=[
+                                block.to_pddl_predicate()
+                            ])
+                            tf2.writeln()
+
+                        #cell contains nothing
+                        if world.is_empty(row=cell.y, col=cell.x):
+                            Clause.write_predicate(f, name="clear", value=[self.cell_predicate(cell)])
+                        tf2.writeln()
+
+                        #adjacent cells
                         adjacent_cell_callbacks = [world.get_up, world.get_down, world.get_left, world.get_right]
                         for adjacent_cell_callback in adjacent_cell_callbacks:
                             try:
-                                next_cell = adjacent_cell_callback(*cell)
-                                if world.is_traversable(*next_cell):
+                                next_cell = adjacent_cell_callback(row=cell.y, col=cell.x)
+                                if world.is_traversable(row=next_cell.y, col=next_cell.x):
                                     Clause.write_predicate(f,
-                                        name="adjacent",
+                                        name="MOVE-DIR",
                                         value=[
                                             self.cell_predicate(cell),
-                                            self.cell_predicate(next_cell)
+                                            self.cell_predicate(next_cell),
+                                            self.get_direction(cell, next_cell).parsable_str,
                                         ])
-                                    f.write("\n")
+                                    tf2.writeln()
                             except ValueError as e:
                                 # there is no such cell
                                 pass
 
-                with Clause(f, indent=1, name="goal", colon=True):
-                    with Clause(f, name="and", fake=len(world.goals) == 1):
-                        for p in world.goals:
-                            Clause.write_predicate(f, name="has_box", value=self.cell_predicate(p))
-                            f.write("\n")
+                with Clause(f, indent=1, name="goal", colon=True) as tf2:
+                    with Clause(f, name="and", fake=len(world.goals) == 1) as tf3:
+                        for b in world.blocks:
+                            Clause.write_predicate(f, name="at-goal", value=b.to_pddl_predicate())
+                            tf3.write("\n")
 
         return os.path.abspath(problem_filename)
--- Server/planner_wrapper/planner_wrapper/sokoban_world.py
+++ Server/planner_wrapper/planner_wrapper/sokoban_world.py
@@ -18,13 +18,30 @@ class BaseCellContent(ParsableEnum):
     UNTRAVERSABLE = BaseCellContentTuple(character="U")
 
 
+class Block:
+
+    def __init__(self, theid: int, position: Point):
+        self.id = theid
+        self.position = position
+
+    def to_pddl_predicate(self) -> str:
+        return "stone-{:02d}".format(self.id)
+
+
 class SokobanWorld:
+    """
+    Represents the sokoban world. It is a decorated matrix. (0,0) is in the topleft corner of the matrix.
+    DOWN is when the y tends to increase in module.
+    """
+
     def __init__(self, rows, columns):
         self._m = [set() for _ in range(rows*columns)]
         self._rows = rows
         self._colums = columns
         self._docking_station = None
         self._robot = None
+        self._blocks = []
+        self._next_block_id = 0
 
     def _2d21d(self, row, col):
         return row * self.cols + col
@@ -35,9 +52,12 @@ class SokobanWorld:
     def add_content_in(self, row, col, content: BaseCellContent):
         self._m[self._2d21d(row, col)].add(content)
         if content == BaseCellContent.ROBOT:
-            self._robot = Point(row, col)
+            self._robot = Point(y=row, x=col)
         if content == BaseCellContent.DOCKING_STATION:
-            self._docking_station = Point(row, col)
+            self._docking_station = Point(y=row, x=col)
+        if content == BaseCellContent.BLOCK:
+            self._blocks.append(Block(theid=self._next_block_id, position=Point(y=row, x=col)))
+            self._next_block_id += 1
 
     def remove_content_in(self, row, col, content: BaseCellContent):
         self._m[self._2d21d(row, col)].remove(content)
@@ -45,16 +65,34 @@ class SokobanWorld:
     def contains_content(self, row, col, content: BaseCellContent):
         return content in self._m[self._2d21d(row, col)]
 
-    def __getitem__(self, item: typing.Tuple[int, int]) -> typing.List[BaseCellContent]:
-        row, col = item
-        return self._m[self._2d21d(row, col)]
+    def __getitem__(self, item: Point) -> typing.List[BaseCellContent]:
+        return self._m[self._2d21d(row=item.y, col=item.x)]
 
-    def __setitem__(self, key: typing.Tuple[int, int], value: typing.List[BaseCellContent]):
-        row, col = key
-        self._m[self._2d21d(row, col)] = value
+    def __setitem__(self, key: Point, value: typing.List[BaseCellContent]):
+        self._m[self._2d21d(row=key.y, col=key.x)] = value
 
     def is_traversable(self, row, col):
-        return BaseCellContent.UNTRAVERSABLE not in self[row, col]
+        return BaseCellContent.UNTRAVERSABLE not in self[Point(y=row, x=col)]
+
+    def is_empty(self, row, col):
+        """
+
+        :param row: the row of the cell involved
+        :param col: the column of the cell involved
+        :return: False if in the cell there is a ROBOT, UNTRAVERSABLE or a BLOCK; true otherwise or if there is nothing
+        """
+
+        if BaseCellContent.UNTRAVERSABLE in self[Point(y=row, x=col)]:
+            return False
+        if BaseCellContent.BLOCK in self[Point(y=row, x=col)]:
+            return False
+        if BaseCellContent.ROBOT in self[Point(y=row, x=col)]:
+            return False
+        if BaseCellContent.DOCKING_STATION in self[Point(y=row, x=col)]:
+            return True # goal doesn't count for clear cell
+        if BaseCellContent.GOAL in self[Point(y=row, x=col)]:
+            return True # goal doesn't count for clear cell
+        return len(self[Point(y=row, x=col)]) == 0
 
     def get_up(self, row, col):
         if row == 0:
@@ -101,12 +139,8 @@ class SokobanWorld:
         return (Point(x=t[1], y=t[0]) for t in itertools.product(range(0, self.rows), range(0, self.cols)))
 
     @property
-    def blocks(self) -> typing.Iterable[Point]:
-        ret_val = []
-        for index in range(self.cells_number):
-            if BaseCellContent.BLOCK in self[self._1d22d(index)]:
-                ret_val.append(self._1d22d(index))
-        return ret_val
+    def blocks(self) -> typing.Iterable[Block]:
+        return self._blocks
 
     @property
     def goals(self) -> typing.Iterable[Point]:

b2da57804cf638bbc3b203507ce7207229127838
Massimo Bono <mbono@unibs.it>
2018-03-23 11:59:54 +0100

    add exploration actions

---
+++ Server/planner_wrapper/planner_wrapper/domains/exploration/exploration_actions.py
@@ -0,0 +1,10 @@
+from planner_wrapper.domains.sokoban.sokoban_actions import Direction, SokobanMove
+from planner_wrapper.interfaces import IPlannerAction
+from planner_wrapper.utils import Point, ParsableEnum
+
+
+class ExplorationMove(SokobanMove):
+    """
+    0:   (MOVE PLAYER-01 POS-05-08 POS-04-08 DIR-LEFT) [1]
+    """
+    pass
